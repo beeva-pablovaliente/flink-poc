2288222e0f3cfa310b7e507201a17a7bee611016
pabloantonio.valiente <pabloantonio.valiente@beeva.com>
2016-04-10 12:30:01 +0200

    Initial commit with a basic example of flink stream

---
+++ src/main/java/com/beeva/flink/FlinkApp.java
@@ -0,0 +1,57 @@
+package com.beeva.flink;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ *
+ * Created by rekkeb on 10/4/16.
+ */
+@SpringBootApplication
+public class FlinkApp implements CommandLineRunner {
+
+    private final Logger logger = LoggerFactory.getLogger(FlinkApp.class);
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 9999)
+                .flatMap(new Splitter())
+                .keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1);
+
+        dataStream.print();
+
+        env.execute("Window WordCount");
+
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word: sentence.split(" ")) {
+                out.collect(new Tuple2<>(word, 1));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(FlinkApp.class).bannerMode(Banner.Mode.OFF).run(args);
+    }
+
+}
